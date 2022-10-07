598fe3eb4ffcc04c43ddd8c3d44a07d60b233c67
LysonPrinz <Eril.yilmaz@hotmail.com>
2022-10-07 11:19:05 +0200

    added Products Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { Container} from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import { useEffect } from 'react';
 import { ThemeProvider } from "@mui/system";
 import theme from './styles/theme'
 import Navbar from './components/navbar';
 import Banner from './components/banner';
 import Slider from './components/promotions';
-import { Product } from './styles/products';
+import Products from './components/products';
+
 
 
 
@@ -13,18 +14,21 @@ function App() {
 
   useEffect(() => {
     document.title = "ShoppingCart - Home"
-  },[]);
+  }, []);
 
 
   return (
-    <ThemeProvider theme ={theme}>
-          <Container maxWidth="xl" sx={{ background: '#fff'}}
-    >
-      <Navbar />
-      <Banner/>
-      <Slider />
-      <Product />
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="xl" sx={{ background: '#fff' }}
+      >
+        <Navbar />
+        <Banner />
+        <Slider />
+        <Box display="flex" justifyContent="center" sx={{ p: 4 }}>
+          <Typography variant="h4">Unsere Produkte</Typography>
+        </Box>
+        <Products />
+      </Container>
     </ThemeProvider>
 
   );
