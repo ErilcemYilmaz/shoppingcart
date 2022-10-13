b37403b11d42aaa3af81f116b22ec29a2b509174
LysonPrinz <Eril.yilmaz@hotmail.com>
2022-10-13 15:59:39 +0200

    added Cart

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Footer from './components/footer';
 import AppDrawer from './components/drawer';
 import { UIProvider } from './context/ui';
 import SearchBox from './components/search';
+import Cart from './components/cart';
 
 
 
@@ -35,6 +36,7 @@ function App() {
           <Products />
           <Footer />
           <AppDrawer />
+          <Cart />  
           <SearchBox />
         </UIProvider>
 
