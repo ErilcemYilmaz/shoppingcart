361a4d00339c1b778cb387b1c483e23f961d9dac
LysonPrinz <Eril.yilmaz@hotmail.com>
2022-10-13 16:00:21 +0200

    adjustment for cart

--- src/components/navbar/actions.js
+++ src/components/navbar/actions.js
@@ -1,13 +1,17 @@
-import { Divider, ListItemButton, ListItemIcon } from "@mui/material";
+import { Badge, Divider, ListItemButton, ListItemIcon } from "@mui/material";
 import { NavbarList, ActionIconsContainerMobile, ActionIconsContainerDesktop } from "../../styles/navbar";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PersonIcon from "@mui/icons-material/Person"
 import FavoriteIcon from "@mui/icons-material/Favorite"
 import { Colors } from "../../styles/theme";
 
-export default function Actions({matches}) {
+import { useUIContext } from "../../context/ui";
+
+export default function Actions({ matches }) {
 
     const Component = matches ? ActionIconsContainerMobile : ActionIconsContainerDesktop;
+
+    const { cart, setShowCart } = useUIContext();
 
 
     return (
@@ -15,12 +19,14 @@ export default function Actions({matches}) {
             <NavbarList type="row">
                 <ListItemButton sx={{ justifyContent: 'center' }}>
                     <ListItemIcon sx={{ display: 'flex', justifyContent: 'center', color: matches && Colors.secondary }}>
-                        <ShoppingCartIcon />
+                        <Badge badgeContent={cart && cart.length} color="secondary">
+                            <ShoppingCartIcon onClick={() => setShowCart(true)}/>
+                        </Badge>
                     </ListItemIcon>
                 </ListItemButton>
                 <Divider orientation="vertical" flexItem />
                 <ListItemButton sx={{ justifyContent: 'center' }}>
-                    <ListItemIcon sx={{ display: 'flex', justifyContent: 'center', color: matches && Colors.secondary}}>
+                    <ListItemIcon sx={{ display: 'flex', justifyContent: 'center', color: matches && Colors.secondary }}>
                         <FavoriteIcon />
                     </ListItemIcon>
                 </ListItemButton>
