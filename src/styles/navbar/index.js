3f40f04d39c7065a883a19a2bb3200f588f5715f
LysonPrinz <eril.yilmaz@hotmail.com>
2022-10-16 16:59:24 +0200

    added class SearchIcon

--- src/styles/navbar/index.js
+++ src/styles/navbar/index.js
@@ -1,5 +1,5 @@
 import { IconButton, List, Typography } from "@mui/material";
-import {styled} from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 import { Box } from "@mui/system";
 import "@fontsource/montez";
 import { Colors, DrawerWidth } from "../theme";
@@ -7,44 +7,49 @@ import { Colors, DrawerWidth } from "../theme";
 
 //Container
 export const NavbarContainer = styled(Box)(() => ({
-    display: 'flex',
-    marginTop: 4,
-    justifyContent: 'center',
+  display: 'flex',
+  marginTop: 4,
+  justifyContent: 'center',
   alignItems: 'center',
-    padding: '2px 8px'
-
+  padding: '2px 8px',
+  ".SearchIcon": {
+    color: Colors.secondary, 
+    borderBottom: `1px solid ${Colors.secondary}`,
+   }
+   
 }));
 
 
 //header
 export const NavbarHeader = styled(Typography)(() => ({
-    padding: "4px",
-    flexGrow: 1,
-    fontSize: "4em",
-    fontFamily: '"Montez", "cursive"',
-    color: Colors.secondary,
-    // "&:hover": {
-    //   animation: `${textPopUpTop} 0.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) both`,
-    // },
-  }));
+  padding: "4px",
+  flexGrow: 1,
+  fontSize: "4em",
+  fontFamily: '"Montez", "cursive"',
+  color: Colors.secondary,
+  // "&:hover": {
+  //   animation: `${textPopUpTop} 0.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) both`,
+  // },
+}));
 
 
 
 export const NavbarList = styled(List)(({ type }) => ({
-    display: type === "row" ? "flex" : "block",
-    flexGrow: 3,
-    justifyContent: "center",
-    alignItems: "center",
+  display: type === "row" ? "flex" : "block",
+  flexGrow: 3,
+  justifyContent: "center",
+  alignItems: "center",
+  color: Colors.white,
 }));
 
 
-export const ActionIconsContainerDesktop = styled(Box) (() =>({
+export const ActionIconsContainerDesktop = styled(Box)(() => ({
 
   flexGrow: 0,
-
+  
 }));
 
-export const ActionIconsContainerMobile = styled(Box) (() =>({
+export const ActionIconsContainerMobile = styled(Box)(() => ({
   display: 'flex',
   background: Colors.shaft,
   position: "fixed",
@@ -52,24 +57,25 @@ export const ActionIconsContainerMobile = styled(Box) (() =>({
   left: 0,
   width: '100%',
   alignItems: 'center',
-  zIndex: 99,  
-  borderTop: `1px solid ${Colors.border}`
-
+  zIndex: 99,
+  borderTop: `1px solid ${Colors.border}`,
 }));
 
 
 export const MyList = styled(List)(({ type }) => ({
   display: type === "row" ? "flex" : "block",
   flexGrow: 3,
-justifyContent: "center",
-alignItems: "center",
+  justifyContent: "center",
+  alignItems: "center",
 }));
 
 
 
 export const DrawerCloseButton = styled(IconButton)(() => ({
-position: 'absolute',
-top: 10,
-left: DrawerWidth,
-zIndex: 1999,      
+  position: 'absolute',
+  top: 10,
+  left: DrawerWidth,
+  zIndex: 1999,
 }));
+
+
