4dbea7513eef3ef08c34e34f6382fa1ee7473a76
LysonPrinz <eril.yilmaz@hotmail.com>
2022-10-16 16:58:46 +0200

    added new products

--- src/fake_data/index.js
+++ src/fake_data/index.js
@@ -2,36 +2,86 @@ export const products = [
     {
         id: 1,
         name: "Adalya Tabak",
-        price: 129.99,
+        price: "25.00",
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "/images/products/adalya_tabak.jpg"
+        image: "/images/products/adalya_tabak.png"
     },
     {
         id: 2,
         name: "Black Nana",
-        price: 199.99,
+        price: "25.00",
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "/images/products/black_nana.jpg"
+        image: "/images/products/black_nana.png"
     },
     {
         id: 3,
         name: "Lady Killer",
-        price: 189.99,
+        price: "25.00",
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "/images/products/lady_killer.jpg"
+        image: "/images/products/lady_killer.png"
     },
     {
         id: 4,
         name: "Lemon Chill",
-        price: 129.99,
+        price: "25.00",
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "/images/products/lemon_chill.jpg"
+        image: "/images/products/lemon_chill.png"
     },
     {
         id: 5,
         name: "Os Tabacco",
-        price: 399.99,
+        price: "25.00",
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "/images/products/ostobacco.jpg"
+        image: "/images/products/ostobacco.png"
     },
+    {
+        id: 6,
+        name: "Almasiva Bruderherz",
+        price: "25.00",
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/almasiva_bruderherz.png"
+    },
+    {
+        id: 7,
+        name: "Erdbeeren-Mango Teller gross",
+        price: "12.00",
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/fruits1.png"
+    },
+    {
+        id: 8,
+        name: "Erdbeeren-Mango Teller klein",
+        price: "7.00",
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/fruits1.png"
+    },
+    {
+        id: 9,
+        name: "Käse Schinken Sandwich",
+        price: "8.00",
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/kaese_schinken.png"
+    },
+    {
+        id: 10,
+        name: "Lipton Ice Tea Lemon",
+        price: "5.50",
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/lipton-lemon-50cl.png"
+    },
+    {
+        id: 11,
+        name: "Lipton Ice Tea Peach",
+        price: "5.50",
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/lipton_peach_50cl.png"
+    },
+    {
+        id: 12,
+        name: "CocaCola 50cl",
+        price: "5.50",
+        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+        image: "/images/products/coca_cola_50cl.png"
+    },
+
 ]
