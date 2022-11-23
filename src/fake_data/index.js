97b30c5433cb1126a1bd928764b10ed418f7d3b1
Erilcem Yilmaz <erilcem.yilmaz@numas.ch>
2022-11-23 16:33:31 +0100

    added category, 1=Tabak 2=Food 3=Drinks

--- src/fake_data/index.js
+++ src/fake_data/index.js
@@ -3,83 +3,95 @@ export const products = [
         id: 1,
         name: "Adalya Tabak",
         price: "25.00",
+        category: 1,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "/images/products/adalya_tabak.png"
     },
     {
         id: 2,
         name: "Black Nana",
-        price: "25.00",
+        price: 25.00,
+        category: 1,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "/images/products/black_nana.png"
     },
     {
         id: 3,
         name: "Lady Killer",
-        price: "25.00",
+        price: 25.00,
+        category: 1,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "/images/products/lady_killer.png"
     },
     {
         id: 4,
         name: "Lemon Chill",
-        price: "25.00",
+        price: 25.00,
+        category: 1,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "/images/products/lemon_chill.png"
     },
     {
         id: 5,
         name: "Os Tabacco",
-        price: "25.00",
+        price: 25.00,
+        category: 1,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "/images/products/ostobacco.png"
     },
     {
         id: 6,
         name: "Almasiva Bruderherz",
-        price: "25.00",
+        price: 25.00,
+        category: 1,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "/images/products/almasiva_bruderherz.png"
     },
     {
         id: 7,
         name: "Erdbeeren-Mango Teller gross",
-        price: "12.00",
+        price: 12.00,
+        category: 2,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "/images/products/fruits1.png"
     },
     {
         id: 8,
         name: "Erdbeeren-Mango Teller klein",
-        price: "7.00",
+        price: 7.00,
+        category: 2,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "/images/products/fruits1.png"
     },
     {
         id: 9,
         name: "Käse Schinken Sandwich",
-        price: "8.00",
+        price: 8.00,
+        category: 2,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "/images/products/kaese_schinken.png"
     },
     {
         id: 10,
         name: "Lipton Ice Tea Lemon",
-        price: "5.50",
+        price: 5.50,
+        category: 3,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "/images/products/lipton-lemon-50cl.png"
     },
     {
         id: 11,
         name: "Lipton Ice Tea Peach",
-        price: "5.50",
+        price: 5.50,
+        category: 3,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "/images/products/lipton_peach_50cl.png"
     },
     {
         id: 12,
         name: "CocaCola 50cl",
-        price: "5.50",
+        price: 5.50,
+        category: 3,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "/images/products/coca_cola_50cl.png"
     },
