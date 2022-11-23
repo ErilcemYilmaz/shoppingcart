e2398e02d76614a90b4cf86bb47c3b69588ff081
Erilcem Yilmaz <erilcem.yilmaz@numas.ch>
2022-11-23 16:30:55 +0100

    correction, added Total for each Item

--- src/components/ui/indec/index.js
+++ src/components/ui/indec/index.js
@@ -1,21 +1,22 @@
 import { IconButton, Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import { Box } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from "@mui/icons-material/Remove";
 import { useState } from "react";
 import { clamp } from "./clamp";
 import { Colors } from "../../../styles/theme";
 
-export default function IncDec() {
+
+export default function IncDec({product}) {
     const clampV = clamp(1, 10);
     const [value, setValue] = useState(1);
-
+    const priceAfter = value * product.price;
     return (
       <Box display="flex">
         <IconButton
           sx={{
             borderRadius: 0,
-            background: `${Colors.secondary}`,
+            color: `${Colors.secondary}`,
           }}
           onClick={() => setValue(clampV(value - 1))}
         >
@@ -24,7 +25,7 @@ export default function IncDec() {
         <Typography
           variant="h6"
           sx={{
-            border: `1px solid ${Colors.secondary}`,
+
             p: 2,
           }}
         >
@@ -33,12 +34,17 @@ export default function IncDec() {
         <IconButton
           sx={{
             borderRadius: 0,
-            background: `${Colors.secondary}`,
+              color: `${Colors.secondary}`,
           }}
           onClick={() => setValue(clampV(value + 1))}
         >
           <AddIcon />
         </IconButton>
+            <Typography id="test" sx={{ p: 3,
+            }}  key={product.id}>Total: {( priceAfter ).toFixed(2)}</Typography>
+            <br/>
       </Box>
+
+
     );
 }
