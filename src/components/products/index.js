ae016d5579b06e77a869b784d3eeddb0d477d82c
Erilcem Yilmaz <erilcem.yilmaz@numas.ch>
2023-01-18 14:13:05 +0100

    added filtering to render Products

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -3,38 +3,58 @@ import { Container, Grid, useMediaQuery } from "@mui/material";
 import { products } from "../../fake_data"
 import SingleProduct from "./SingleProduct";
 import SingleProductDesktop from "./SingleProductDesktop";
+import { useState } from "react";
+import Select from '@mui/material/Select';
 
 
 export default function Products() {
 
     const theme = useTheme();
     const matches = useMediaQuery(theme.breakpoints.down('md'));
+    const [category, setCategory] = useState("all");
+    const [selectValue, setSelectValue] = useState("all");
 
-    const renderProducts = products.map(product => (
-        <Grid item key={product.id}
-            xs={2}
-            sm={4}
-            md={4}
-            display="flex" flexDirection={"column"} alignItems="center"
-        >
-            {matches ? (
-                <SingleProduct product={product} matches={matches} />
-            ) : (
-                <SingleProductDesktop product={product} matches={matches} />
-            )}
+    let filteredProduct = products.filter(product => product.categories === category || category === 'all');
 
-        </Grid>
-    ));
+    let categories = new Set(products.map(product => product.categories))
 
     return (
         <Container>
+            <Select
+                native
+                value={selectValue}
+                onChange={(e) => {
+                    setSelectValue(e.target.value);
+                    setCategory(e.target.value);
+                }}
+            >
+                <option value="all">All</option>
+                {Array.from(categories).map((category) => (
+                    <option key={category} value={category}>
+                        {category}
+                    </option>
+                ))}
+            </Select>
             <Grid container
                 spacing={{ xs: 3, md: 4 }}
                 justifyContent={"center"}
                 sx={{ margin: "20px 4px 10px 4px" }}
                 columns={{ xs: 2, sm: 8, md: 12 }}
             >
-                {renderProducts}
+                {filteredProduct.map((product) => (
+                    <Grid item key={product.id}
+                        xs={2}
+                        sm={4}
+                        md={4}
+                        display="flex" flexDirection={"column"} alignItems="center"
+                    >
+                        {matches ? (
+                            <SingleProduct product={product} matches={matches} category={category} setCategory={setCategory} />
+                        ) : (
+                            <SingleProductDesktop product={product} matches={matches} category={category} setCategory={setCategory} />
+                        )}
+                    </Grid>
+                ))}
             </Grid>
         </Container>
     );
